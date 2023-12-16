80de2f528a35817a0ff6f9aa1b533ec4c307f31f
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2023-12-16 21:33:48 +0200

    backend works 95%
    left out auth, tomorrow that but QT GUI first tomorrow

--- Node/app.js
+++ Node/app.js
@@ -1,52 +1,56 @@
 //16.12.2023
 //Antti Suomi, syksy 2023 ohjelmaprojekti rästitehtävä
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 // enable json
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cookieParser());
 // allow for  x-www-form-urlencoded
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // top level
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 // books database
-const bookRouter = require('./routes/book');
-app.use('/book', bookRouter);
+const bookRouter = require("./routes/book");
+app.use("/book", bookRouter);
+//car and qt
+const carRouter = require("./routes/car");
+app.use("/car", carRouter);
+
 //stop injections and allow cross site scripting
-const helmet = require('helmet');
-const cors = require('cors');
-//get token ignore signins
-const jwt = require('jsonwebtoken');
+const helmet = require("helmet");
+const cors = require("cors");
+// //get token ignore signins
+// const jwt = require('jsonwebtoken');
 
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+// function authenticateToken(req, res, next) {
+//     const authHeader = req.headers['authorization']
+//     const token = authHeader && authHeader.split(' ')[1]
 
-    console.log("token = "+token);
-    if (token == null) return res.sendStatus(401)
+//     console.log("token = "+token);
+//     if (token == null) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
-      console.log(err)
+//     jwt.verify(token, process.env.MY_TOKEN, (err, user) => {
+//       console.log(err)
 
-      if (err) return res.sendStatus(403)
+//       if (err) return res.sendStatus(403)
 
-      req.user = user
+//       req.user = user
 
-      next()
-    })
-  }
+//       next()
+//     })
+//   }
 
 app.use(helmet());
 app.use(cors());
