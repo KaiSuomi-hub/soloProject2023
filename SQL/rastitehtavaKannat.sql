1b2648b03a4622078c38f1e6e54fddf4aea9ae3d
KaiSuomi-hub <69754493+KaiSuomi-hub@users.noreply.github.com>
2023-12-16 15:28:18 +0200

    dd

--- SQL/rastitehtavaKannat.sql
+++ SQL/rastitehtavaKannat.sql
@@ -16,10 +16,12 @@
 
 
 -- Dumping database structure for tietokannat
+DROP DATABASE IF EXISTS `tietokannat`;
 CREATE DATABASE IF NOT EXISTS `tietokannat` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `tietokannat`;
 
 -- Dumping structure for table tietokannat.rastibook
+DROP TABLE IF EXISTS `rastibook`;
 CREATE TABLE IF NOT EXISTS `rastibook` (
   `id_book` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -31,7 +33,20 @@ CREATE TABLE IF NOT EXISTS `rastibook` (
 
 -- Dumping data for table tietokannat.rastibook: ~0 rows (approximately)
 
+-- Dumping structure for table tietokannat.rasticar
+DROP TABLE IF EXISTS `rasticar`;
+CREATE TABLE IF NOT EXISTS `rasticar` (
+  `int_car` int NOT NULL AUTO_INCREMENT,
+  `branch` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  PRIMARY KEY (`int_car`),
+  KEY `int_car` (`int_car`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Rästitehtävä table #2';
+
+-- Dumping data for table tietokannat.rasticar: ~0 rows (approximately)
+
 -- Dumping structure for table tietokannat.rastiuser
+DROP TABLE IF EXISTS `rastiuser`;
 CREATE TABLE IF NOT EXISTS `rastiuser` (
   `is_user` int NOT NULL AUTO_INCREMENT,
   `username` varchar(20) NOT NULL,
@@ -39,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `rastiuser` (
   PRIMARY KEY (`is_user`),
   UNIQUE KEY `username` (`username`),
   KEY `is_user` (`is_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Rästitehtävä table #3, this is accessible with the QT app.';
 
 -- Dumping data for table tietokannat.rastiuser: ~0 rows (approximately)
 
